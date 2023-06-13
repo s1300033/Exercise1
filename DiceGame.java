98c5315ac2265b21fa04a2a73771d81851a1b509
s1300033 <136042340+s1300033@users.noreply.github.com>
2023-06-13 12:36:03 +0900

    Dice game: initial revision

---
+++ DiceGame.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+class DiceGame{
+    public static void main(String args[]){
+	Random rand = new Random();
+	int saikoro1 = rand.nextInt(6) + 1;
+	int saikoro2 = rand.nextInt(6) + 1;
+
+	System.out.println("Rolling dice...");
+	System.out.println("Die 1:"+ saikoro1);
+	System.out.println("Die 2:"+ saikoro2);
+	System.out.println("Total value:"+(saikoro1 + saikoro2));
+	
+	
+    }
+}
